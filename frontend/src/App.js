ce775414dc7b2a0796111f467287ac737f4b0ed1
Prechyious <lemoprechy@gmail.com>
2023-07-11 20:45:21 +0100

    Finished Styling Login Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
-function App() {
-  return (
-    <h1>Hello from App</h1>
-  );
-}
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+
+const App = () => {
+    return (
+        <Switch>
+            <Route path="/login">
+                <LoginFormPage />
+            </Route>
+        </Switch>
+    );
+};
 
 export default App;
